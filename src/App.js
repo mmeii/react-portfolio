fd411f4ef49361f789600f2fa08deca75b45f762
Mengmei Tu <redmmeii@gmail.com>
2021-03-17 12:58:37 -0500

    update home route

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <div className="app">
         <Navbar />
         <Switch>
-          <Route exact path="/react-portfolio" component={Home} />
+          <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
           <Route exact path="/projects" component={Projects} />
           <Route exact path="/contact" component={Contact} />
